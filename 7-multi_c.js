a386aab4033e44bfcb1f4bd0a98e9bf6b53f1a2d
Sandra Naggayi <naggayi.sandra@gmail.com>
2025-07-13 19:53:24 +0000

    creates a 7 word argument

---
+++ 7-multi_c.js
@@ -0,0 +1,9 @@
+const arg = process.argv[2];
+const num = parseInt(arg);
+if (Number.isNaN(num) || num < 0) {
+    console.log("Missing number of occurrences");
+} else {
+    for (let i = 0; i < num; i++) {
+        console.log("C is fun");
+    }
+}
