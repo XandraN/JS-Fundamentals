9946298b3ecb2bc1e568b4b0b3490328356ae0f6
Sandra Naggayi <naggayi.sandra@gmail.com>
2025-07-13 19:58:52 +0000

    creates 8 square argument

--- 8-square.js
+++ 8-square.js
@@ -1,9 +1,10 @@
+
 const arg = process.argv[2];
 const size = parseInt(arg);
 if (Number.isNaN(size) || size <= 0) {
     console.log("Missing size");
 } else {
-    const row = "X".String(size);
+    const row = "X".repeat(size);
     for (let i = 0; i < size; i++) {
         console.log(row);
     }
